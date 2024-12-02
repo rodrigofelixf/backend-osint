49acaf3663020126b8e6ab44e565106923e45982
rodrigofelixf <rodrigofelixferreira@hotmail.com>
2024-12-02 19:35:40 -0300

    Configura variavel de ambiente

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
 
-DATABASE_URL = "postgresql://start:password@localhost:5433/startdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(DATABASE_URL)
